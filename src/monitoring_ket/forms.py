a6a2724a959670025d6f85e31413cdb53ce8150d
Валерий Вадимович Пальчех <palcheh.v@alnikor.loc>
2025-01-12 13:12:15 +0300

    изм форм

--- src/monitoring_ket/forms.py
+++ src/monitoring_ket/forms.py
@@ -4,10 +4,20 @@ from .models import Monitoring_ket
 class Monitoring_ketForm(forms.ModelForm):
     class Meta:
         model = Monitoring_ket
-        fields = ['date', 'group', 'animal_1', 'animal_2', 'animal_3', 'animal_4',
-              'animal_5', 'animal_6', 'animal_7', 'animal_8', 'animal_9', 'animal_10',
-              'animal_11', 'animal_12', 'animal_13', 'animal_14', 'animal_15',
-              'keton_1', 'keton_2', 'keton_3', 'keton_4',
-              'keton_5', 'keton_6', 'keton_7', 'keton_8', 'keton_9', 'keton_10',
-              'keton_11', 'keton_12', 'keton_13', 'keton_14', 'keton_15',
-              'recommendations', 'job', 'user_name']
+        fields = ['date', 'group',
+                  'animal_1', 'keton_1',
+                  'animal_2', 'keton_2',
+                  'animal_3', 'keton_3',
+                  'animal_4', 'keton_4',
+                  'animal_5', 'keton_5',
+                  'animal_6', 'keton_6',
+                  'animal_7', 'keton_7',
+                  'animal_8', 'keton_8',
+                  'animal_9', 'keton_9',
+                  'animal_10', 'keton_10',
+                  'animal_11', 'keton_11',
+                  'animal_12', 'keton_12',
+                  'animal_13', 'keton_13',
+                  'animal_14', 'keton_14',
+                  'animal_15', 'keton_15',
+                  'recommendations', 'job', 'user_name']
--- src/monitoring_ph/forms.py
+++ src/monitoring_ph/forms.py
@@ -5,7 +5,14 @@ class Monitoring_phForm(forms.ModelForm):
     class Meta:
         model = Monitoring_ph
         fields = ['date', 'group',
-                  'animal_1', 'animal_2', 'animal_3', 'animal_4', 'animal_5', 'animal_6', 'animal_7', 'animal_8', 'animal_9', 'animal_10',
-                  'calving_1', 'calving_2', 'calving_3', 'calving_4', 'calving_5', 'calving_6', 'calving_7', 'calving_8', 'calving_9', 'calving_10',
-                  'ph_1', 'ph_2', 'ph_3', 'ph_4', 'ph_5', 'ph_6', 'ph_7', 'ph_8', 'ph_9', 'ph_10',
+                  'animal_1', 'calving_1', 'ph_1',
+                  'animal_2', 'calving_2', 'ph_2',
+                  'animal_3', 'calving_3', 'ph_3',
+                  'animal_4', 'calving_4', 'ph_4', 
+                  'animal_5', 'calving_5', 'ph_5',
+                  'animal_6', 'calving_6', 'ph_6',
+                  'animal_7', 'calving_7', 'ph_7',
+                  'animal_8', 'calving_8', 'ph_8',
+                  'animal_9', 'calving_9', 'ph_9', 
+                  'animal_10', 'calving_10', 'ph_10',
                   'recommendations', 'job', 'user_name']
